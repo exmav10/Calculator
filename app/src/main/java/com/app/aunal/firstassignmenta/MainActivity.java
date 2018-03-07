8809387d765d6df02f084f767a53c0ab2c98726d
aydinunal <hands_up_@hotmail.co.uk>
2018-03-07 23:59:14 +0300

    layout added.

--- app/src/main/java/com/app/aunal/firstassignmenta/MainActivity.java
+++ app/src/main/java/com/app/aunal/firstassignmenta/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     @BindView(R.id.textView) TextView textView;
@@ -31,118 +32,140 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.root) Button root;
     @BindView(R.id.power) Button power;
     private String operation = "";
+    int startInt = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setClickListeners();
     }
     private void setClickListeners(){
+
         seven.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + seven.getText();
+                textView.setText(operation);
             }
         });
         eight.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + eight.getText();
+                textView.setText(operation);
             }
         });
         nine.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + nine.getText();
+                textView.setText(operation);
             }
         });
         six.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + six.getText();
+                textView.setText(operation);
             }
         });
         five.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + five.getText();
+                textView.setText(operation);
             }
         });
         four.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + four.getText();
+                textView.setText(operation);
             }
         });
         three.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + three.getText();
+                textView.setText(operation);
             }
         });
         two.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + two.getText();
+                textView.setText(operation);
             }
         });
         one.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + one.getText();
+                textView.setText(operation);
             }
         });
         zero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + zero.getText();
+                textView.setText(operation);
             }
         });
         point.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + point.getText();
+                textView.setText(operation);
             }
         });
         equal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                System.out.println("this is operation = " + operation);
             }
         });
         divide.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                operation = operation + divide.getText();
+                textView.setText(operation);
 
             }
         });
         multiply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + divide.getText();
+                textView.setText(operation);
             }
         });
         substract.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + substract.getText();
+                textView.setText(operation);
             }
         });
         plus.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + plus.getText();
+                textView.setText(operation);
             }
         });
         root.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + root.getText();
+                textView.setText(operation);
             }
         });
         power.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                operation = operation + power.getText();
+                textView.setText(operation);
             }
         });
         back.setOnClickListener(new View.OnClickListener() {
@@ -158,4 +181,5 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
 }
